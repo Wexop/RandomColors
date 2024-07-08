f25e8d54d182d17f0c33612e9944aadc512ccec7
Wexop <mattheurtin@gmail.com>
2024-07-08 15:39:54 +0200

    fix flashlight in pocket

---
+++ Patchs/PatchFlashLight.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace RandomColors.Patchs;
+
+[HarmonyPatch(typeof(FlashlightItem))]
+public class PatchFlashLight
+{
+    [HarmonyPatch(nameof(FlashlightItem.PocketItem))]
+    [HarmonyPostfix]
+    private static void PatchTurnOnFlashLight(FlashlightItem __instance)
+    {
+        if (!__instance.isBeingUsed || !RandomColorsPlugin.instance.affectFlashLight.Value) return;
+
+        var lightColor = __instance.gameObject.GetComponentInChildren<Light>()?.color;
+
+        if (!lightColor.HasValue) return;
+
+        var helmetLight =
+            GetChildGameObject(GameNetworkManager.Instance.localPlayerController.gameObject, "HelmetLights");
+
+        var lights = helmetLight.GetComponentsInChildren<Light>(true);
+
+        foreach (var l in lights) l.color = lightColor.Value;
+    }
+
+    public static GameObject GetChildGameObject(GameObject fromGameObject, string withName)
+    {
+        var ts = fromGameObject.GetComponentsInChildren<Transform>(true);
+        foreach (var t in ts)
+            if (t.gameObject.name == withName)
+                return t.gameObject;
+        return null;
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -79,6 +79,7 @@ public class RandomColorsPlugin : BaseUnityPlugin
         Harmony.CreateAndPatchAll(typeof(PatchRoundManager));
         Harmony.CreateAndPatchAll(typeof(PatchGrabbableObject));
         Harmony.CreateAndPatchAll(typeof(PatchEnemyAi));
+        Harmony.CreateAndPatchAll(typeof(PatchFlashLight));
         try
         {
             Harmony.CreateAndPatchAll(typeof(PatchVehiculeControler));
